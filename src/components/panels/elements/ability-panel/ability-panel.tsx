c092f40c3609e3dab1142c93ce9d4bdfb8c07bf7
Primal Zed <primal.zed@gmail.com>
2024-12-30 10:23:33 -0600

    hook for persisted options

--- src/components/panels/elements/ability-panel/ability-panel.tsx
+++ src/components/panels/elements/ability-panel/ability-panel.tsx
@@ -8,24 +8,25 @@ import { HeaderText } from '../../../controls/header-text/header-text';
 import { Hero } from '../../../../models/hero';
 import { HeroLogic } from '../../../../logic/hero-logic';
 import { HeroicResourceBadge } from '../../../controls/heroic-resource-badge/heroic-resource-badge';
-import { Options } from '../../../../models/options';
 import { PanelMode } from '../../../../enums/panel-mode';
 import { PowerRollPanel } from '../../power-roll/power-roll-panel';
 import { Tag } from 'antd';
 import { Utils } from '../../../../utils/utils';
+import { usePersistedOptions } from '../../../../hooks/use-persisted-options';
 
 import './ability-panel.scss';
 
 interface Props {
 	ability: Ability;
 	hero?: Hero;
-	options?: Options;
 	mode?: PanelMode;
 	tags?: string[];
 	onRoll?: () => void;
 }
 
 export const AbilityPanel = (props: Props) => {
+	const { options } = usePersistedOptions();
+
 	try {
 		let cost = props.ability.cost;
 		let disabled = false;
@@ -39,7 +40,7 @@ export const AbilityPanel = (props: Props) => {
 
 			cost = Math.max(cost, 1);
 
-			if (props.options?.dimUnavailableAbilities || false) {
+			if (options?.dimUnavailableAbilities || false) {
 				disabled = cost > props.hero.state.heroicResource;
 			}
 		}
@@ -95,7 +96,7 @@ export const AbilityPanel = (props: Props) => {
 								props.ability.spend.map((spend, n) => (
 									<Field
 										key={n}
-										disabled={props.hero && (props.options?.dimUnavailableAbilities || false) && (spend.value > props.hero.state.heroicResource)}
+										disabled={props.hero && (options?.dimUnavailableAbilities || false) && (spend.value > props.hero.state.heroicResource)}
 										label={(
 											<div style={{ display: 'inline-flex',  alignItems: 'center', gap: '5px' }}>
 												<span>Spend</span>
